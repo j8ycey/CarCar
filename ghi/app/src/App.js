2fd71b2bebe15800da606c9756b0bd068d1ef034
Jonathan <yoo.y.jonathan@gmail.com>
2022-08-02 09:23:58 -0700

    filtering is finally working

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import InventoryList from './InventoryList';
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/inventory" element={<InventoryList />} />
+          {/* <Route path="/manufacturers/new" element={<ManufacturerForm />} />
+          <Route path="/models" element={<Models />} />
+          <Route path="/models/new" element={<ModelForm />} />
+          <Route path="/cars" element={<Cars />} />
+          <Route path="/cars/new" element={<CarForm />} /> */}
         </Routes>
       </div>
     </BrowserRouter>
